91b5e8b436b774ae56ee1afa44e2ed54cc136bb1
Meikle, Braeden Andrew <bmeikle3@gatech.edu>
2025-12-29 22:44:18 -0500

    Copy over action player

--- site/src/App.js
+++ site/src/App.js
@@ -2,6 +2,12 @@ import './styles/global.css'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import Projects from './pages/Projects'
+
+import ActionPlayerHome from '../../action-player/src/pages/home'
+import ActionPlayerDownload from '../../action-player/src/pages/download'
+
+import BZDevHome from '../../bz-dev/src/page/bzdevhome'
+
 import PremiumSwiftHome from '../../premium-swift/src/pages/home'
 
 function App() {
@@ -11,6 +17,7 @@ function App() {
         <Route path='/' element={<Home/>} />
         <Route path='/projects' element={<Projects/>}/>
         <Route path='/projects/action-player' element={<ActionPlayerHome/>}/>
+        <Route path='/projects/action-player/download' element={<ActionPlayerDownload/>}/>
         <Route path='/projects/bz-dev' element={<BZDevHome/>}/>
         <Route path='/projects/premium-swift' element={<PremiumSwiftHome/>}/>
       </Routes>
