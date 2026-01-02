1c3f99f55a989e3b873c3e221f520c4cd56a738d
Meikle, Braeden Andrew <bmeikle3@gatech.edu>
2026-01-02 15:34:49 -0500

    Update activity

--- src/proj/premium-swift/src/cmp/Title.jsx
+++ src/proj/premium-swift/src/cmp/Title.jsx
@@ -2,7 +2,6 @@ function Title() {
   return (
     <pre style={{
       fontSize: '24px',
-      padding: 0,
       color: 'rgb(174, 0, 255)',
       background: 'rgb(13,13,13)', 
       padding: 20,
